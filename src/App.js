c0e672011276f260bfa415408ff1b0c364d9d21d
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-03 21:10:50 +0600

    Removed some files from src folder and add some css code into index.css folder, and also practice some basics on App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = 'sayem'
+  const x = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h1>React Task Tracker</h1>
+      <h2>hello {x ? 'yes' : 'no'}</h2>
+    </div> 
+    
   );
 }
 
