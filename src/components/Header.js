50c6065e270c726130c2093f69bfa0ac3cde1425
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-03 22:23:40 +0600

    Practicing onClick event

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 const Header = ({ title }) => {
+
+  const onClick = () => {
+    console.log ('Clicked')
+  }
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add '/>
-      <Button color='red ' text='Close '/>
-      <Button color='black' text='Hello '/>
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
