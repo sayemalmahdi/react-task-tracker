2ca045816b750ce491def6e3a2209b5c2343eaf5
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-03 23:49:22 +0600

    Output Tasks on UI using maps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from 'react'
 
 function App() {
-  
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'December 03 2022 at 09:00 am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'December 03 2022 at 11:00 am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping ',
+        day: 'December 04 2022 at 03:00 pm',
+        reminder: false,
+    }
+] )  
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div> 
     
   );
