1d84f002e376a2f5711f0779a8a6e4d004daeecf
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-05 21:13:42 +0600

    Simply-Done Add Task Submit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import {useState} from 'react'
 
 function App() {
@@ -35,9 +36,17 @@ const toggleReminder = (id) => {
   setTasks (tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
 }
 
+// Add Task
+   const addTask = (task) => {
+    const id = Math.floor(Math.random() *1000) +1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask ])
+   }
+
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Task Remain'}
     </div> 
     
