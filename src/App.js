003fc93cad2ef3fa5f17f87d3c5d6bc26f5f198b
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-05 21:21:20 +0600

    Worked on showAddTask State

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddTask from "./components/AddTask";
 import {useState} from 'react'
 
 function App() {
+  const [showAddTask,setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -37,16 +38,16 @@ const toggleReminder = (id) => {
 }
 
 // Add Task
-   const addTask = (task) => {
-    const id = Math.floor(Math.random() *1000) +1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask ])
-   }
+  const addTask = (task) => {
+  const id = Math.floor(Math.random() *1000) +1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask ])
+  }
 
   return (
     <div className="container">
       <Header />
-      <AddTask onAdd={addTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Task Remain'}
     </div> 
     
