17a247ecd25eba071dd66306170d08a7f42c613a
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-12 21:40:33 +0600

    JSON Server

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import {useState} from 'react'
 
 function App() {
   const [showAddTask,setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'December 03 2022 at 09:00 am',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'December 03 2022 at 11:00 am',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping ',
-        day: 'December 04 2022 at 03:00 pm',
-        reminder: false,
-    }
-])
+  const [tasks, setTasks] = useState([])
 
 // Delete Task
 const deleteTask = (id) => {
