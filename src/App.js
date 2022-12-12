e0fff9b8490da771129e76713aecc1c794f0ac67
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-12 23:10:16 +0600

    Toggle Reminder On Server Done

--- src/App.js
+++ src/App.js
@@ -24,6 +24,15 @@ function App() {
     return data 
   }
 
+//Fetech Task singular
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+
+  return data
+}
+
+
 // Delete Task
 const deleteTask = async (id) => {
   await fetch (`http://localhost:5000/tasks/${id}`, {
@@ -34,9 +43,22 @@ const deleteTask = async (id) => {
 }
 
 // Toggle Reminder
-const toggleReminder = (id) => {
+const toggleReminder = async(id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(updTask),
+  })
+
+  const data = await res.json()
+
   // console.log (id)
-  setTasks (tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  setTasks (tasks.map((task) => task.id === id ? {...task, reminder:data.reminder} : task))
 }
 
 // Add Task
