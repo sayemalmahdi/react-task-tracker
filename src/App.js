b65f5250805285955f57326cfb32da12ff1826ae
Sayem Mohammed Al-Mahdi <sayemmohammedal-mahdi@Sayems-MacBook-Pro.local>
2022-12-12 22:28:40 +0600

    Add Task To Server Done

--- src/App.js
+++ src/App.js
@@ -40,10 +40,23 @@ const toggleReminder = (id) => {
 }
 
 // Add Task
-  const addTask = (task) => {
-  const id = Math.floor(Math.random() *1000) +1
-  const newTask = { id, ...task }
-  setTasks([...tasks, newTask ])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+
+  // const id = Math.floor(Math.random() *1000) +1
+  // const newTask = { id, ...task }
+  // setTasks([...tasks, newTask ])
   }
 
   return (
